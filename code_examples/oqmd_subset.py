1485e78dc3692fa72a256525d23cc98527433818
wilsonnater <46790713+wilsonnater@users.noreply.github.com>
2019-02-18 14:38:32 -0600

    Update oqmd_subset.py

--- code_examples/oqmd_subset.py
+++ code_examples/oqmd_subset.py
@@ -1,3 +1,9 @@
+##Code grabs CIF files and then puts them into
+##Pymatgen structure object using built in pymatgen
+##tool. Then checks if number of sites is between 
+##2 and 10 and if the strcutre includes any atoms that
+##it was told to avoid. Is made for linux operating system
+
 from pymatgen.core import structure
 import numpy as np
 import os
@@ -6,7 +12,7 @@ rootdir = 'nate_oqmd_data'
 
 extensions = ('.cif')
 finaldir = 'sub_oqmd_data'
-
+#atoms to avoid
 avoid = np.append(np.append(np.array([1 , 2, 10, 18,36, 54, 86]),np.arange(57,72)),np.arange(84,113))
 
 for subdir, dirs, files in os.walk(rootdir):
@@ -16,15 +22,15 @@ for subdir, dirs, files in os.walk(rootdir):
         if ext in extensions:
             go = True
             ciffile = os.path.join(subdir, dfile)
-            struct=structure.IStructure.from_file(ciffile)
-            if 2<struct.num_sites<10:
-                npavoid = np.unique(np.array(struct.atomic_numbers))
-                for i in npavoid:
-                    if i in avoid:
+            struct=structure.IStructure.from_file(ciffile) #Creates structure object from CIF
+            if 2<struct.num_sites<10:   #Checks if number of sites is between 2 and 10
+                npavoid = np.unique(np.array(struct.atomic_numbers)) #gets unique atomic numbers
+                for i in npavoid: #loops over all unique atomic numbers
+                    if i in avoid:   #the atomic number is in avoid sets go to flase
                         go = False
             else:
                 go=False
             if go:
                 jsonf=(top+'.json')
-                shutil.copy(ciffile,os.path.join(finaldir, dfile))
-                shutil.copy(os.path.join(subdir, jsonf),os.path.join(finaldir, jsonf))
+                shutil.copy(ciffile,os.path.join(finaldir, dfile)) ##copies CIF
+                shutil.copy(os.path.join(subdir, jsonf),os.path.join(finaldir, jsonf)) ##copies JSON
