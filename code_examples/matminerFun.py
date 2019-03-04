d1e8fcb59a9343dcd047f99653b22f0e0ec6b9bb
wilsonnater <46790713+wilsonnater@users.noreply.github.com>
2019-03-04 12:21:37 -0600

    Update matminerFun.py

--- code_examples/matminerFun.py
+++ code_examples/matminerFun.py
@@ -9,4 +9,8 @@ df_mp = MPDataRetrieval("y6hicvzKBaLRWuG8").get_dataframe(criteria={"task_id":{"
 print(type(df_mp)) #pandas dataframe
 print(df_mp.iloc[0]) #outputs the following: structure    [[0. 0. 0.] Na, [2.32362417 1.64305041 4.02463...
                      #                       Name: mp-22862, dtype: object
-
+# Needed to go one level deeper in df_mp
+print(df_mp.iloc[0][0])
+# If you look at the type of file that is it is pymatgen.structure.Structure object
+print(type(df_mp.iloc[0][0]))
+#this is a dataformat we can create
